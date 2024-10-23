87f88f1d471fb86963d2cbc7c95e2f422e39ca3f
Vadim Schultz <vadim.schultz@rohde-schwarz.com>
2024-10-23 13:30:01 +0200

    Add constraints to force FP style.

---
+++ sing-a-song/vadim_fp.py
@@ -0,0 +1,12 @@
+"""
+FP tenets:
+* First-class and higher-order functions
+* Pure functions
+* Recursion
+* Lazy evaluation
+* Referential transparency
+
+Constraints:
+* no loops
+* no assignments
+"""
