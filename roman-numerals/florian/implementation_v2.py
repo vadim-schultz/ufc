0f1d391866fcacd39c6d1e08fbef753d5937d130
TogiraShigeru <florianeckardt91@gmail.com>
2024-11-26 20:10:50 +0100

    Add error handling

--- roman-numerals/florian/implementation_v2.py
+++ roman-numerals/florian/implementation_v2.py
@@ -1,26 +1,18 @@
 from functools import reduce
 import typing as ty
 import numpy as np
+from dataclasses import dataclass
 
 
+@dataclass
 class Literal:
+    value_corrected: int
+    value: int
+    char: str
 
-    def __init__(self, value_corrected: int, value: int, char: str) -> None:
-        self._value_corrected = value_corrected
-        self._value = value
-        self._char = char
-
-    @property
-    def value_corrected(self) -> int:
-        return self._value_corrected
-
-    @property
-    def value(self) -> int:
-        return self._value
-
-    @property
-    def char(self) -> str:
-        return self._char
+    @classmethod
+    def from_char(cls, char: str) -> "Literal":
+        return next(filter(lambda x: x.char == char, all_literals))
 
 
 all_literals = [
@@ -40,9 +32,22 @@ all_literals = [
 ]
 
 
+repeats = [3, 1, 1, 1] * 3 + [3]
+max_literals = [literal for literal, repeat in zip(all_literals, repeats) for _ in range(repeat)]
+
+
 class Number:
     def __init__(self, source: ty.Union[str, int]) -> None:
         self.source = source
+
+        if isinstance(self.source, str):
+            self._validate_roman()
+
+        if isinstance(self.source, int):
+            self._validate_int()
+
+        if self.source is None:
+            raise ValueError(f"Invalid Integer: {self.source}. Must be an integer or a Roman Numeral.")
 
     @property
     def integer(self) -> int:
@@ -62,9 +67,7 @@ class Number:
 
     def from_roman(self, roman: str) -> int:
         counts = [roman.count(literal.char) for literal in all_literals]
-        t = sum((count * literal.value_corrected for literal, count in zip(all_literals, counts)))
-
-        return t
+        return sum((count * literal.value_corrected for literal, count in zip(all_literals, counts)))
 
     def to_roman(self, integer: int) -> str:
 
@@ -72,9 +75,39 @@ class Number:
             a.append(b.char) if self.from_roman("".join(a + [b.char])) <= integer else None
             return a
 
-        idx = np.concatenate([np.array([0, 0, 0, 1]) + 2 * i for i in range(7)])[:-1]
-        max_literals = list(map(lambda idx: all_literals[idx], idx))
         return "".join(reduce(accu, max_literals[::-1], []))
+
+    def _validate_int(self):
+
+        if not isinstance(self.source, int):
+            raise ValueError(f"Invalid Integer: {self.source}. Must be an integer.")
+
+        if self.source < 1 or self.source > 3000:
+            raise ValueError(f"Invalid Integer: {self.source}. Must be between 1 and 3000.")
+
+    def _validate_roman(self):
+        if len(self.source) < 1:
+            raise ValueError(f"Invalid Roman Numeral: {self.source}. Must be at least 1 character long.")
+
+        if self.from_roman(self.source) > 3000:
+            raise ValueError(f"Invalid Roman Numeral: {self.source}. Must be 3000 or less.")
+
+        if any(char not in map(lambda x: x.char, all_literals) for char in self.source):
+            raise ValueError(f"Invalid Roman Numeral: {self.source}. Contains non-Roman characters")
+
+        counts = [self.source.count(literal.char) for literal in all_literals]
+        if any(count > 3 for count in counts):
+            raise ValueError(f"Invalid Roman Numeral: {self.source}. Not more than 3 repeats allowed.")
+
+        if any(count > 1 and literal.char not in ["I", "X", "C", "M"] for literal, count in zip(all_literals, counts)):
+            raise ValueError(f"Invalid Roman Numeral: {self.source}. Not all literals can be repeated.")
+
+        for char0, char1 in zip(self.source, self.source[1:]):
+
+            lit0, lit1 = Literal.from_char(char0), Literal.from_char(char1)
+            if lit0.value < lit1.value:
+                if char0 + char1 not in list(map(lambda x: x.char, all_literals)):
+                    raise ValueError(f"Invalid Roman Numeral: {self.source}. Invalid substraction.")
 
 
 def from_roman(roman: str) -> int:
@@ -85,8 +118,8 @@ def to_roman(integer: int) -> str:
     return Number(integer).roman
 
 
-roman = "M"
-integer = 1000
-num = Number(integer)
-num.roman
-e = 1
+# roman = "M"
+# integer = None
+# num = Number(integer)
+# num.roman
+# e = 1
