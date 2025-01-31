74f2d9fa55c23dd2818377f14a2af0a21a1509a9
TogiraShigeru <florianeckardt91@gmail.com>
2025-01-31 15:12:15 +0100

    Add matches played stats

--- ttt/utils.py
+++ ttt/utils.py
@@ -29,17 +29,22 @@ def standings_from_results(results, players):
     standings = []
     for player in players:
         score = 0
+        matches_played = 0
         left_side_instances = [result for result in results if result.player0 == player or result.player1 == player]
         right_side_instances = [result for result in results if result.player2 == player or result.player3 == player]
 
         # Add scores to each side
         for matches in left_side_instances:
             score += matches.score0
+            if matches.score0 != 0 or matches.score1 != 0:
+                matches_played += 1
 
         for matches in right_side_instances:
             score += matches.score1
+            if matches.score0 != 0 or matches.score1 != 0:
+                matches_played += 1
 
-        standings.append({"name": player, "points": score})
+        standings.append({"name": player, "points": score, "matches_played": matches_played})
 
     # Sort list by points
     standings.sort(key=lambda x: x["points"], reverse=True)
